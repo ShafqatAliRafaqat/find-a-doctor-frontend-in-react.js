5120b76e893906fcf3f588a31355979c742e52bb
ShafqatAliRafaqat <Shafqat.Ali.786@outlook.com>
2019-11-29 11:30:36 +0500

    11-29-2019

--- src/Components/ContactUs/contact.jsx
+++ src/Components/ContactUs/contact.jsx
@@ -63,7 +63,7 @@ class ContactUs extends Component {
 
 	render() {
 		const {name,email,phone,message} 	=	this.state;
-			if(window.location.href == "http://demo.hospitallcare.com/#/contact") {
+			if(window.location.href == "https://demo.hospitallcare.com/#/contact") {
 				window.location = window.location.href + '/';
 				window.location.reload();
 			}
