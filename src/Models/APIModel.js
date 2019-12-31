ee09277b3ad045474ebeb5ea939f5c9ee58358b5
unknown <Administrator@Nspire013.netsolpk.com>
2019-12-31 17:07:53 +0500

    12-31-2019

--- src/Models/APIModel.js
+++ src/Models/APIModel.js
@@ -1,7 +1,7 @@
 class APIModel {
     // static HOST  = "http://127.0.0.1:80/api/v1/en/";
     // static HOST  = "http://192.168.10.122:8080/PerformanceSoftwareV2/public/api/v1/en/";
-    // static HOST     = "https://support.hospitallcare.com/api/websiteapi/w1/";
-    static HOST     = "http://localhost:8000/api/websiteapi/w1/";
+    static HOST     = "https://support.hospitallcare.com/api/websiteapi/w1/";
+    // static HOST     = "http://localhost:8000/api/websiteapi/w1/";
   } 
   export default APIModel;
