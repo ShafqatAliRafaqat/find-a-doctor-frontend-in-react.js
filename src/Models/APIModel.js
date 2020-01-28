a55e1edf0f85ce8b42f019d626462dab5b5f4296
malikzeeshaan <shanimalikshan@gmail.com>
2020-01-28 15:50:24 +0500

    28-01-2020

--- src/Models/APIModel.js
+++ src/Models/APIModel.js
@@ -1,6 +1,6 @@
 class APIModel {
 
-    static HOST     = "https://support.hospitallcare.com/api/websiteapi/w1/";
-    // static HOST     = "http://localhost:8000/api/websiteapi/w1/";
+    // static HOST     = "https://support.hospitallcare.com/api/websiteapi/w1/";
+    static HOST     = "http://localhost:8000/api/websiteapi/w1/";
   } 
   export default APIModel;
