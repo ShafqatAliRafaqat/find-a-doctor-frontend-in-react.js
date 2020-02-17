01293eadc6e426a9344331d423de6aa36d1cc2fd
ShafqatAliRafaqat <Shafqat.Ali.786@outlook.com>
2020-02-17 09:15:04 +0500

    02-17-2020

--- src/Components/Clinic/detail.jsx
+++ src/Components/Clinic/detail.jsx
@@ -487,7 +487,7 @@ class Detail extends Component{
 						<meta charSet="utf-8" />
 						<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 						<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-						<meta name="description" content="{center_data.name} | List of top doctors in {center_data.name}- Book an appointment with top doctors" />
+						<meta name="description" content='`${center_data.name}`| List of top doctors in `${center_data.name}`- Book an appointment with top doctors' />
 						<meta name="author" content="Hospitall Care" />
 						<title>{center_data.name} | Top Doctors In {center_data.name} - Book an appointment with top doctors of {center_data.name}</title>
 						<Link to={{pathname:`/center-detail/${slugify(center_data.name)}/${center_data.id}`}}></Link>
