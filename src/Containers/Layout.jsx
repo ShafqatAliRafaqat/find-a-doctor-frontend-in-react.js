894336bfbc224da3dff4be7de681542803b3e133
unknown <Administrator@Nspire013.netsolpk.com>
2020-03-11 05:11:50 -0700

    03-11-2020

--- src/Containers/Layout.jsx
+++ src/Containers/Layout.jsx
@@ -42,7 +42,7 @@ class Layout extends Component {
                                   <Route key={idx} path={route.path} exact={route.exact} name={route.name} render={props => {
                                     if(route.path){
                                           return (
-                                            <div style={{minHeight:"436px" , marginTop:"64px"}}>
+                                            <div style={{minHeight:"436px"}}>
                                               <route.Component alertify={alertify} {...props} {...this.props} errorHandler={this.errorHandler} />                                  
                                             </div>  
                                           );
