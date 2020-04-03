1a70451b3e2c4ed757119a96f6eaddf39dd81b99
ShafqatAliRafaqat <Shafqat.Ali.786@outlook.com>
2020-04-03 19:00:10 +0500

    04-03-2020

---
+++ build/precache-manifest.b83c8ce49aba482cd5889621001def2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f765be33d01767614c2d00f4dd0f5937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57edc4bfa02b1a288939",
+    "url": "/static/css/2.7dfa9170.chunk.css"
+  },
+  {
+    "revision": "57edc4bfa02b1a288939",
+    "url": "/static/js/2.c0140756.chunk.js"
+  },
+  {
+    "revision": "3697d0a3e31f15548792",
+    "url": "/static/js/main.f2d46f2f.chunk.js"
+  },
+  {
+    "revision": "4a1f195e60388e069386",
+    "url": "/static/js/runtime-main.237dd709.js"
+  }
+]);
